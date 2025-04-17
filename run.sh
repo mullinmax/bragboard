9e9388cc734e9e3b02bf086cbfc82479f5af8cac
Maxwell Mullin <mullinmax@gmail.com>
2025-04-17 17:41:52 -0400

    pi qwirks

--- run.sh
+++ run.sh
@@ -2,10 +2,10 @@
 compose_file="compose.yml"
 
 echo "Stopping and removing containers..."
-docker compose -f $compose_file down
+sudo docker compose -f $compose_file down
 
 echo "Building ingest and webui services..."
-docker compose -f $compose_file up --build --remove-orphans
+sudo docker compose -f $compose_file up --build --remove-orphans
 
 echo "Stopping and removing containers..."
-docker compose -f $compose_file down
+sudo docker compose -f $compose_file down
